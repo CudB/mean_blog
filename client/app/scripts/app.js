16bac3c7771d7ea47a09e4ed962f3b77956bb10c
Andy Au <andyau.aa@gmail.com>
2016-04-04 15:23:14 -0700

    added blog route

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -24,6 +24,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/blog', {
+        templateUrl: 'views/blog.html',
+        controller: 'BlogCtrl',
+        controllerAs: 'blog'
+      })
       .otherwise({
         redirectTo: '/'
       });
