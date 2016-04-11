a243ffec2f84ca9cd5d836c825ebfb6f0d39af88
Andy Au <andyau.aa@gmail.com>
2016-04-11 13:35:52 -0700

    create post page functional

--- client/app/scripts/controllers/post-add.js
+++ client/app/scripts/controllers/post-add.js
@@ -11,8 +11,10 @@ angular.module('clientApp')
   .controller('PostAddCtrl', function ($scope, Post, $location) {
     $scope.post = {};
     $scope.savePost = function() {
+      $scope.post.date = new Date(); 
+      $scope.post.status = 'draft';
       Post.post($scope.post).then(function() {
-        $location.path('/posts');
+        $location.path('/blog');
       });
     };
   });
