b755f1b77909cefa22fa7af6a222e86af4ee08eb
Andy Au <andyau.aa@gmail.com>
2016-04-14 15:48:23 -0700

    issue #1 fixed: editting posts updates post.tags

--- client/app/scripts/controllers/post-edit.js
+++ client/app/scripts/controllers/post-edit.js
@@ -13,10 +13,9 @@ angular.module('clientApp')
     $scope.post = {};
     Post.one($routeParams.id).get().then(function(post) {
       $scope.post = post;
-      $scope.tags = $scope.post.tags.join();
+      $scope.post.tags = $scope.post.tags.join();
       $scope.savePost = function() {
-        $scope.post.tags = $scope.tags.replace(/[\s]/g, '').split(',');
-        console.log($scope.tags);
+        $scope.post.tags = $scope.post.tags.replace(/[\s]/g, '').split(',');
         $scope.post.save().then(function() {
           $location.path('/post/' + $routeParams.id);
         });
