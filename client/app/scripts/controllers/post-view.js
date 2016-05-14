d43b0d66f5978cdb3c3a83ce50611fcc283eba21
Andy Au <andyau.aa@gmail.com>
2016-05-14 14:27:10 -0700

    added alert for successful comment

--- client/app/scripts/controllers/post-view.js
+++ client/app/scripts/controllers/post-view.js
@@ -12,7 +12,8 @@ angular.module('clientApp')
     Post.one($routeParams.id).get().then(function(post) {
       $scope.post = post;
     }, function(response) {
-      // 404 page
+      // add appropriate error page
+      console.log("Error with status code", response.status);
     });
     $scope.form = {};
     $scope.comment = {};
@@ -20,8 +21,12 @@ angular.module('clientApp')
       $scope.comment.date = new Date();
       $scope.post.comments.push($scope.comment);
       $scope.post.put().then(function() {
+        $scope.showSuccessAlert = true;
         $scope.comment = {};
         $scope.form.addComment.$setPristine();
+      }, function(response) {
+        // add appropriate error banner
+        console.log("Error with status code", response.status);
       });
     };
   });
